5499041cc7f57d52093e570fd7b5cb992c640515
dong.wang <dong.wang@yingzt.com>
2019-03-12 15:55:29 +0800

    Initial Commit

---
+++ SlantLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SlantLabel
+//
+//  Created by 王泽龙 on 2019/3/12.
+//  Copyright © 2019 王泽龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SlantLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlantLabel
+//
+//  Created by 王泽龙 on 2019/3/12.
+//  Copyright © 2019 王泽龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
